2ef72999516476dfaed5599418e36932ac6cc6c5
Oleg Kanatov <okanatov@gmail.com>
2018-09-26 00:45:18 +0300

    __str__ method is finished

--- bag/bag.py
+++ bag/bag.py
@@ -4,29 +4,36 @@ class Bag:
     def __init__(self):
         self._vertices = []
 
-
     def add_edge(self, f, t):
-        try: 
-            self._vertices[f]
-        except IndexError:
-            self._vertices.insert(f, [])
-
+        self._try_or_create(f)
         self._vertices[f].append(t)
-
-        try: 
-            self._vertices[t]
-        except IndexError:
-            self._vertices.insert(t, [])
-
+        self._try_or_create(t)
 
     def __str__(self):
-        v = ""
+        string_representation = ""
 
-        for i, vertex in enumerate(self._vertices):
-            v += str(i)
-            v += ": "
-            for edge in vertex:
-                v += str(edge)
-            v += "; "
+        for vertex_index, vertex in enumerate(self._vertices):
+            if vertex_index != 0:
+                string_representation += "; "
 
-        return v
+            string_representation += str(vertex_index)
+            string_representation += ": "
+            string_representation += self._print_edges(vertex)
+
+        return string_representation
+
+    def _print_edges(self, vertex):
+        result = ""
+
+        for edge_index, edge in enumerate(vertex):
+            if edge_index != 0:
+                result += ", "
+            result += str(edge)
+
+        return result
+
+    def _try_or_create(self, index):
+        try:
+            self._vertices[index]
+        except IndexError:
+            self._vertices.insert(index, [])
--- bag/tests/test_bag.py
+++ bag/tests/test_bag.py
@@ -4,11 +4,19 @@ import bag
 class TestBag(unittest.TestCase):
 
     def test_empty_bag(self):
-        b = bag.Bag()
-        self.assertEqual(str(b), "")
+        graph = bag.Bag()
+        self.assertEqual(str(graph), "")
 
     def test_adds_edge(self):
-        b = bag.Bag()
-        b.add_edge(0, 1)
+        graph = bag.Bag()
+        graph.add_edge(0, 1)
 
-        self.assertEqual(str(b), "0: 1; 1: ; ")
+        self.assertEqual(str(graph), "0: 1; 1: ")
+
+    def test_adds_2_edges(self):
+        graph = bag.Bag()
+        graph.add_edge(0, 1)
+        graph.add_edge(0, 2)
+        graph.add_edge(1, 2)
+
+        self.assertEqual(str(graph), "0: 1, 2; 1: 2; 2: ")
