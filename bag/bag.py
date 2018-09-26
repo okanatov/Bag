b1dd2455a1b0452c96d655d1336abec6b795ce04
Oleg Kanatov <okanatov@gmail.com>
2018-09-26 12:45:15 +0300

    Iterator has been added

--- bag/bag.py
+++ bag/bag.py
@@ -1,6 +1,18 @@
-import bag
-
 class Bag:
+    class BagIterator:
+        def __init__(self, collection):
+            self._collection = collection
+            self._pos = 0
+
+        def __next__(self):
+            if self._pos >= len(self._collection):
+                raise StopIteration()
+
+            elem = self._collection[self._pos]
+            self._pos += 1
+
+            return elem
+
     def __init__(self):
         self._vertices = []
 
@@ -37,3 +49,9 @@ class Bag:
             self._vertices[index]
         except IndexError:
             self._vertices.insert(index, [])
+
+    def __len__(self):
+        return len(self._vertices)
+
+    def __iter__(self):
+        return self.BagIterator(self._vertices)
--- bag/tests/test_bag.py
+++ bag/tests/test_bag.py
@@ -2,7 +2,6 @@ import unittest
 import bag
 
 class TestBag(unittest.TestCase):
-
     def test_empty_bag(self):
         graph = bag.Bag()
         self.assertEqual(str(graph), "")
@@ -20,3 +19,23 @@ class TestBag(unittest.TestCase):
         graph.add_edge(1, 2)
 
         self.assertEqual(str(graph), "0: 1, 2; 1: 2; 2: ")
+
+    def test_gets_length(self):
+        graph = bag.Bag()
+        graph.add_edge(0, 1)
+        graph.add_edge(0, 2)
+        graph.add_edge(1, 2)
+
+        self.assertEqual(len(graph), 3)
+
+    def test_(self):
+        graph = bag.Bag()
+        graph.add_edge(0, 1)
+        graph.add_edge(0, 2)
+        graph.add_edge(1, 2)
+
+        iterator = iter(graph)
+
+        self.assertEqual(str(next(iterator)), str([1, 2]))
+        self.assertEqual(str(next(iterator)), str([2]))
+        self.assertEqual(str(next(iterator)), str([]))
